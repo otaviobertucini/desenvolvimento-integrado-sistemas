829ddccd00c8e3aa8ad6ec409083d94ae648a648
Otávio Thomas Bertucini <otaviobertucini@gmail.com>
2021-05-11 21:44:36 -0300

    All set.

--- server/server.py
+++ server/server.py
@@ -30,11 +30,11 @@ class CGNE(object):
 
         image = np.zeros((3600, 1))
 
-        print('matrix imported started')
+        # print('matrix imported started')
         matrix_lines = pd.read_csv(str(path) + '/data/real/H-1.txt', sep=',', lineterminator='\n', header=None)
-        print('matrix imported as df')
+        # print('matrix imported as df')
         matrix = np.matrix(matrix_lines.to_numpy())
-        print('natrix converted: ' + str(matrix.dtype))
+        # print('natrix converted: ' + str(matrix.dtype))
 
         r = vector - (matrix * image)
         p = matrix.T * r
@@ -45,11 +45,11 @@ class CGNE(object):
         # while count < 5:
         while error < float('1e-4'):
 
-            print('i = ' + str(count))
+            # print('i = ' + str(count))
 
             alpha = ((r.T * r) / (p.T * p)).item((0, 0))
 
-            print('alpha: ' + str(alpha))
+            # print('alpha: ' + str(alpha))
 
             next_image = image + (alpha * p)
             next_r = r - (alpha * (matrix * p))
@@ -59,7 +59,7 @@ class CGNE(object):
             p = matrix.T * next_r + beta * p
             image = next_image
             error = np.linalg.norm(next_r, 2) - np.linalg.norm(r, 2)
-            print('error: ', error)
+            # print('error: ', error)
             r = next_r 
 
             count += 1
@@ -77,9 +77,10 @@ class CGNE(object):
             "algorithm": "CGNE",
             "start": str(start),
             "end": str(datetime.now()),
+            "iterations": count - 1,
         }
 
-        os.setxattr(image_path, 'user.meta', bytes(json.dumps(attributes)))
+        os.setxattr(image_path, 'user.meta', bytes(json.dumps(attributes),  "ASCII"))
 
         self.is_running = 0
 
@@ -89,10 +90,40 @@ class CGNE(object):
 
 class MyServer(BaseHTTPRequestHandler):
 
+    def do_OPTIONS(self):
+
+        self.send_response(200)
+        self.send_header("Content-Type", "text")
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header("Access-Control-Allow-Credentials", "true")  
+        self.send_header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT")
+        self.send_header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
+        self.end_headers()
+
     def do_GET(self):
 
         print('hello rsrs')
         print('tá rodando?: ', cgne.isRunning())
+
+        image_path = str(pathlib.Path(__file__).parent.absolute()) + '/../images/'
+        result = []
+        files = os.listdir("../images")
+        for f in files:
+            attrs = os.getxattr(image_path + str(f), 'user.meta')
+            result.append({
+                "name": f,
+                "attributes": json.loads(attrs.decode("ASCII")),
+                "path": image_path,
+                "size": os.path.getsize(image_path + str(f))
+            })
+
+        print(result)
+        self.send_response(200)
+        self.send_header("Content-Type", "application/javascript")
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
+        self.end_headers()
+        self.wfile.write(bytes(str(json.dumps(result)), "ASCII"))
 
     def do_POST(self):
         data = json.loads(self.rfile.read(int(self.headers['Content-Length'])))
@@ -100,8 +131,9 @@ class MyServer(BaseHTTPRequestHandler):
         cgne.generate_image(data['name'], data['vector'], data['username'])
 
         self.send_response(200)
-        print("testes")
-        self.send_header("Content-type", "text")
+        self.send_header("Content-Type", "text")
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
         self.end_headers()
         self.wfile.write(bytes("returned", "utf-8"))
 
--- server/test.py
+++ server/test.py
@@ -1,29 +1,31 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib.colors as col
-import pathlib
-from datetime import datetime
+# import numpy as np
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# import matplotlib.colors as col
+# import pathlib
+# from datetime import datetime
 
-start = datetime.now()
+# start = datetime.now()
 
-path = str(pathlib.Path(__file__).parent.absolute()) + '/../images/'
-print('path: ' + path)
+# path = str(pathlib.Path(__file__).parent.absolute()) + '/../images/'
+# print('path: ' + path)
 
-image = pd.read_csv('./image.csv').to_numpy()
-image = np.flipud(np.rot90(image))
+# image = pd.read_csv('./image.csv').to_numpy()
+# image = np.flipud(np.rot90(image))
 
-image = (image - np.min(image))/np.ptp(image)
-png = plt.imsave(path + 'image.png', image, cmap='gray')
+# image = (image - np.min(image))/np.ptp(image)
+# png = plt.imsave(path + 'image.png', image, cmap='gray')
 
-attributes = {
-    "username": 'otavio',
-    "algorithm": "CGNE",
-    "start": str(start),
-    "end": str(datetime.now()),
-}
+# attributes = {
+#     "username": 'otavio',
+#     "algorithm": "CGNE",
+#     "start": str(start),
+#     "end": str(datetime.now()),
+# }
 
-print(attributes)
+# print(attributes)
+
+print(bytes('ola', "ASCII"))
 
 # plt.imsave('image.png', png)
 
